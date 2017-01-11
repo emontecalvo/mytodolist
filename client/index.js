e7201c4c908480e24b3efc2c2eb47549980f1bbc
emontecalvo <emilymontecalvo@gmail.com>
2017-01-11 14:33:59 -0800

    code cleanup'

--- client/index.js
+++ client/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import MainComponent from './main-component';
 import Home from './Home';
 import store from './store';
 import {Provider} from 'react-redux';
